721d5c04c7c26b77fb7759d0860326664ecdd589
Adam Lamar <adamonduty@gmail.com>
2010-01-01 19:58:36 +0000

    Tests for paragraphs and newlines

--- test/formatting_test.rb
+++ test/formatting_test.rb
@@ -24,4 +24,21 @@ class FormattingTest < Test::Unit::TestCase
     assert_match("<b><i>bold & italic</i></b>", markup.to_html)
   end
 
+  def test_paragraphs
+    text = "paragraph 1\n\nparagraph 2\n\nparagraph 3"
+    markup = Marker.parse text
+
+    assert_match("<p>paragraph 1</p><p>paragraph 2</p><p>paragraph 3</p>", markup.to_html)
+  end
+
+  # TODO: verify that the asserted behavior is the desired. MediaWiki
+  # leaves the newlines without a <br/>, but I think that is unintuitive,
+  # especially when pasting text from other sources 
+  def test_newlines
+    text = "line 1\nline 2\nline 3"
+    markup = Marker.parse text
+
+    assert_match("<p>line 1<br/>line 2<br/>line 3</p>", markup.to_html)
+  end
+
 end
