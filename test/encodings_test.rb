b49a79a7f3177e7d93f2648c2f1596035742fa3c
Ryan Blue <rdblue@gmail.com>
2010-04-11 16:51:22 +0200

    Adding test file for encodings.
    Right now, it just has tests for different newline styles (Linux, Windows,
    Mac).  It will eventually have tests for unicode character support.

---
+++ test/encodings_test.rb
@@ -0,0 +1,24 @@
+require File.expand_path( File.dirname( __FILE__ ) + '/test_helper' )
+require 'marker'
+
+class EncodingsTest < Test::Unit::TestCase
+
+  def test_linux_newlines
+    text = "one\ntwo\n\nthree"
+    markup = Marker.parse text
+    assert_match("<p>one two</p>\n\n<p>three</p>", markup.to_html)
+  end
+
+  def test_windows_newlines
+    text = "one\r\ntwo\r\n\r\nthree"
+    markup = Marker.parse text
+    assert_match("<p>one two</p>\n\n<p>three</p>", markup.to_html)
+  end
+
+  def test_mac_newlines
+    text = "one\rtwo\r\rthree"
+    markup = Marker.parse text
+    assert_match("<p>one two</p>\n\n<p>three</p>", markup.to_html)
+  end
+
+end
