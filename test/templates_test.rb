c9a713cdecb1417ecdddf209c0e5916e89954db0
Ryan Blue <rdblue@gmail.com>
2010-11-19 23:02:07 +0100

    Updating the templates tests to include options.
    In previous commits, the options were not part of the default render because
    they included a :tree option that was the parse tree for the template.  This
    was just a hack, so it is now removed and options should be verified in the
    tests as well.

--- test/templates_test.rb
+++ test/templates_test.rb
@@ -3,18 +3,20 @@ require 'marker'
 
 class TemplatesTest < Test::Unit::TestCase
 
+  DEFAULT_OPTIONS = '{:link_base=>"", :footnotes=>}'
+
   def test_basic_template
     text = "{{ template }}"
     markup = Marker.parse text
     
-    assert_match("<p>render:template( :html, [], {} )</p>", markup.to_html)
+    assert_match("<p>render:template( :html, [], {}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
   end
 
   def test_positional_args
     text = "{{ template | one | two | three }}"
     markup = Marker.parse text
     
-    assert_match("<p>render:template( :html, [\"one\", \"two\", \"three\"], {} )</p>", markup.to_html)
+    assert_match("<p>render:template( :html, [\"one\", \"two\", \"three\"], {}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
   end
 
   def test_named_args
@@ -25,31 +27,31 @@ class TemplatesTest < Test::Unit::TestCase
     assert_match("<p>render:template( :html, [], {", markup.to_html)
     assert_match("\"two\"=>\"2\"", markup.to_html)
     assert_match("\"one\"=>\"1\"", markup.to_html)
-    assert_match("} )</p>", markup.to_html)
+    assert_match("}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
   end
 
   def test_mixed_args
     text = "{{ template | one | two = 2 | three }}"
     markup = Marker.parse text
     
-    assert_match("<p>render:template( :html, [\"one\", \"three\"], {\"two\"=>\"2\"} )</p>", markup.to_html)
+    assert_match("<p>render:template( :html, [\"one\", \"three\"], {\"two\"=>\"2\"}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
   end
 
   def test_empty_args
     text = "{{ template | }}"
     markup = Marker.parse text
-    assert_match("<p>render:template( :html, [\"\"], {} )</p>", markup.to_html)
+    assert_match("<p>render:template( :html, [\"\"], {}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
 
     text = "{{ template | | }}"
     markup = Marker.parse text
-    assert_match("<p>render:template( :html, [\"\", \"\"], {} )</p>", markup.to_html)
+    assert_match("<p>render:template( :html, [\"\", \"\"], {}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
   end
 
   def test_rendered_args
     text = "{{ template | [http://www.example.com Example] }}"
     markup = Marker.parse text
 
-    assert_match("<p>render:template( :html, [\"<a href='http://www.example.com'>Example</a>\"], {} )</p>", markup.to_html)
+    assert_match("<p>render:template( :html, [\"<a href='http://www.example.com'>Example</a>\"], {}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
   end
 
   def test_escaped_characters
@@ -59,7 +61,11 @@ class TemplatesTest < Test::Unit::TestCase
     text = '{{ template | \| | a \= b | \|a, b\| }}'
     markup = Marker.parse text
 
-    assert_match('<p>render:template( :html, ["|", "a = b", "|a, b|"], {} )</p>', markup.to_html)
+    assert_match("<p>render:template( :html, [\"|\", \"a = b\", \"|a, b|\"], {}, #{DEFAULT_OPTIONS} )</p>", markup.to_html)
+  end
+
+  def test_line_anchored_markup_inside_templates
+    text = "{{ ruby |\n puts 'hello'\n}}"
   end
 
 end
